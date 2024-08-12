e85634b8f83653a7a9e83a3de952e1505d0c90d8
mdrefathossen <refathossen2001@gmail.com>
2024-08-12 15:42:38 +0600

    data get api done

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     //
     public function index(){
+        $products = Product::get();
+        if($products){
+            return ProductResource::collection($products);
+        }
+        else{
+            return response()->json(['message' => 'Products not found'],200  );
+        }
+
+    }
+    public function store(){
+
+    }
+    public function show(){
+
+    }
+    public function update(){
+
+    }
+    public function destroy(){
 
     }
 }
